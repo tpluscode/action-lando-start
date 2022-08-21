6551ce33ddbf1a25a0b451c33d6f780dcea97e60
tpluscode <tpluscode@users.noreply.github.com>
2022-08-21 12:19:17 +0200

    feat: timeout

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const core = require('@actions/core')
 const tc = require('@actions/tool-cache')
 const cp = require('child_process')
+const waitOn = require('wait-on')
 
 async function setup() {
-    // Get version of tool to be installed
-    const version = core.getInput('version');
+    const version = core.getInput('version')
+    const healthcheck = core.getInput('healthcheck')
+    const timeout = core.getInput('timeout')
 
-    // Download the specific version of the tool, e.g. as a tarball
     const landoInstaller = await tc.downloadTool(await getDownloadURL(version));
 
     cp.exec(`sudo dpkg -i ${landoInstaller}`)
+    cp.exec(`lando version`)
+    cp.exec('lando start')
+
+    if (healthcheck) {
+        await waitOn({
+            resources: [healthcheck],
+            timeout: parseInt(timeout, 10)
+        }).catch(e => {
+            console.error(e)
+            process.exit(1)
+        })
+    }
 }
 
 async function getDownloadURL(version) {
